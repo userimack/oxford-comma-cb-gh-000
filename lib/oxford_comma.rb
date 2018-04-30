93bd7a70dc07ce4541b74730ff8dbdeab3ac7ddf
Mahendra Yadav <mahendra.k12@gmail.com>
2018-04-30 12:26:59 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,8 @@
 def oxford_comma(array)
-  
+  if array.length== 1
+    array.first 
+  if array.length == 2 
+    "#{array.first} and #{array.last}"
+  else
   array.slice(0, (array.length -1)).join(", ") + " and #{array.last}"
 end
